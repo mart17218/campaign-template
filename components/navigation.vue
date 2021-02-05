b55ecb3b81d64a068e347678a7db1a7d19bd9bc6
Lucy <martian17218@gmail.com>
2021-02-05 18:32:24 +0800

    ✨ Feat: add material-icons module

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,10 +1,14 @@
 <template>
-  <nav class="navigation"></nav>
+  <nav class="navigation px-4">
+    <div>
+      <span class="material-icons-round text-white">menu</span>
+    </div>
+  </nav>
 </template>
 
 <style lang="scss" scoped>
 .navigation {
-  @apply bg-black;
+  @apply bg-black flex items-center;
   height: 56px;
 }
 </style>
